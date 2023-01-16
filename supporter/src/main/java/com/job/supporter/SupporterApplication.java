8fcf59c666bae07d2a0ac1d4caacd1825441cbde
DESKTOP <hyh835@naver.com>
2023-01-16 22:31:44 +0900

    PC_230116

---
+++ supporter/src/main/java/com/job/supporter/SupporterApplication.java
@@ -0,0 +1,14 @@
+package com.job.supporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupporterApplication.class, args);
+
+	}
+
+}
---
+++ supporter/src/test/java/com/job/supporter/SupporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.job.supporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
